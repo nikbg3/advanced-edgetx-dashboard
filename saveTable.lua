2895f6f4412de68d4e30b97ddae511cf5c946cf8
nikbg3 <ngkolev@hotmail.com>
2024-10-13 12:22:17 +0300

    Update saveTable.lua

--- saveTable.lua
+++ saveTable.lua
@@ -2,47 +2,46 @@
 --Copyright 2016-2018 to Khadija.el.Bekkaoui and/or Entel Consulting BVBA
 --
 
-local table,fileName=...
+local table, fileName = ...
 
 local indent = ""
 
-local function outputTable (tbl)
-  if not (type(tbl)=="table") then 
+local function outputTable(tbl)
+  if not (type(tbl) == "table") then
     return tbl
   end
-  indent=indent .. "   "
-  local res= "\n" .. indent .. "{\n" 
+  indent = indent .. "   "
+  local res = "\n" .. indent .. "{\n"
   for k, v in pairs(tbl) do
     if type(k) == "number" then
-      k="[" .. k .. "]"
+      k = "[" .. k .. "]"
     end
     if type(v) == "table" then
-      res=res .. indent .. k .. " = " .. outputTable(v) .. ",\n" 
+      res = res .. indent .. k .. " = " .. outputTable(v) .. ",\n"
     elseif type(v) == "function" then
     elseif type(v) == "string" then
-      res=res .. indent .. k .. " = '" .. v .. "',\n" 
+      res = res .. indent .. k .. " = '" .. v .. "',\n"
     elseif type(v) == "boolean" then
-      if v then res=res .. indent .. k .. " = true,\n"
-      else res=res .. indent .. k .. " = false,\n"
+      if v then
+        res = res .. indent .. k .. " = true,\n"
+      else
+        res = res .. indent .. k .. " = false,\n"
       end
     else
-      res=res .. indent .. k .. " = " .. v .. ",\n" 
+      res = res .. indent .. k .. " = " .. v .. ",\n"
     end
   end
-  res = res ..   indent .. "}"
-  indent=string.sub(indent,4) 
-  return res 
-end  
+  res = res .. indent .. "}"
+  indent = string.sub(indent, 4)
+  return res
+end
 
 if io and (type(table) == "table") then
-   
   local f = io.open(fileName, "w")
 
 
-  io.write(f,"return " .. outputTable(table))
+  io.write(f, "return " .. outputTable(table))
   io.close(f)
-    
-  f=nil
 
+  f = nil
 end
-
